ec0f7952965f5a05aebc8498494d31086a43eb9a
Julia Lewis <jewlzpl@gmail.com>
2019-09-12 13:36:07 -0700

    The original commit for the helloworld project

---
+++ helloworld.cpp
@@ -0,0 +1,21 @@
+/*Author: Julia Lewis
+  Date: 8/23/19
+  Project Ditto: This program will output "Hello World Project!" and my name.*/
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World Project!\nJulia Lewis\n";
+}
+
+/*
+Hello World Project!
+Julia Lewis
+
+C:\Users\MagginiUser\Desktop\helloworld\Debug\helloworld.exe (process 2484) exited with code 0.
+To automatically close the console when debugging stops, enable Tools->Options->Debugging->Automatically close the console when debugging stops.
+Press any key to close this window . . .
+*/
